b86f9f795a92d97f6e42ba3b592be6d85abb47d6
Vinh Nguyen <vinh@psyventures.com>
2021-10-18 22:57:10 +0100

    init

---
+++ front-page.php
@@ -0,0 +1,274 @@
+<?php get_header(); ?>
+
+<div class="relative flex flex-col-reverse py-16 lg:py-0 lg:flex-col">
+  <div class="w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:py-20 lg:max-w-screen-xl">
+    <div class="mb-0 lg:max-w-lg lg:pr-8 xl:pr-6">
+      <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:text-center">
+        The Best Italian <br/> Gelato in town.
+      </h2>
+      <p class="mb-5 text-base text-gray-700 md:text-lg md:text-center">
+        We are so excited to share the 2021 Great Taste Award results for Gelatorino products. <br/>
+		We pride ourself to confirm 2019 & 2020 results and to enlarge our award-winning selection to two more Gelato flavours this year, 
+		Dark Chocolate and Gianduja!
+      </p>
+      <div class="mb-10 text-center md:mb-16 lg:mb-20">
+        <a
+          href="/"
+          class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-black transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+        >
+          Learn more
+        </a>
+      </div>
+      <div class="flex flex-col items-center">
+        <div class="mb-2 text-sm text-gray-600 md:mb-2">Follow us</div>
+        <div class="flex items-center space-x-4">
+          <div class="flex items-center">
+            <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                <path
+                  d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div class="flex items-center">
+            <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
+                <circle cx="15" cy="15" r="4"></circle>
+                <path
+                  d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div class="flex items-center">
+            <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                <path
+                  d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div class="flex items-center">
+            <a href="/" class="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
+              <svg viewBox="0 0 24 24" fill="currentColor" class="h-6">
+                <path
+                  d="M23.8,7.2c0,0-0.2-1.7-1-2.4c-0.9-1-1.9-1-2.4-1C17,3.6,12,3.6,12,3.6h0c0,0-5,0-8.4,0.2 c-0.5,0.1-1.5,0.1-2.4,1c-0.7,0.7-1,2.4-1,2.4S0,9.1,0,11.1v1.8c0,1.9,0.2,3.9,0.2,3.9s0.2,1.7,1,2.4c0.9,1,2.1,0.9,2.6,1 c1.9,0.2,8.2,0.2,8.2,0.2s5,0,8.4-0.3c0.5-0.1,1.5-0.1,2.4-1c0.7-0.7,1-2.4,1-2.4s0.2-1.9,0.2-3.9v-1.8C24,9.1,23.8,7.2,23.8,7.2z M9.5,15.1l0-6.7l6.5,3.4L9.5,15.1z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="inset-y-0 top-0 right-0 w-full max-w-xl px-4 mx-auto mb-6 md:px-0 lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+    <img
+      class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+      src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/alana-harris-Ri35owZyfNU-unsplash.jpg"
+      alt=""
+    />
+  </div>
+</div>
+
+<!-----ABOUT ME---->
+
+<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div>
+      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        ABOUT US
+      </p>
+    </div>
+    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+      <span class="relative inline-block">
+        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+          <defs>
+            <pattern id="34f481be-159a-4846-821d-9ca19fb6bcc5" x="0" y="0" width=".135" height=".30">
+              <circle cx="1" cy="1" r=".7"></circle>
+            </pattern>
+          </defs>
+          <rect fill="url(#34f481be-159a-4846-821d-9ca19fb6bcc5)" width="52" height="24"></rect>
+        </svg>
+        <span class="relative">The</span>
+      </span>
+    Story behind Gelatorino
+    </h2>
+    <p class="text-base text-gray-700 md:text-lg">
+      You’ll feel like you’re in a little corner of Italy when you head inside a Gelatorino shop. In Italy, gelato provides the perfect end to a meal, and Italians will travel far and wide to sample the best gelato around.
+    </p>
+  </div>
+  <div class="grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-4 sm:grid-cols-2">
+    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+      <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+        <h6 class="mb-2 font-semibold leading-5">Authentic Italian gelato</h6>
+        <p class="text-sm text-gray-900">
+          We are passionate about creating authentic Italian gelato using the best quality, natural, fresh, and indulgent ingredients, that we can find.
+        </p>
+      </div>
+    </div>
+    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+      <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+        <h6 class="mb-2 font-semibold leading-5">Small production</h6>
+        <p class="text-sm text-gray-900">
+          Our artisanal, small production allows us to do what we love! Experimenting with flavours, and techniques to produce exciting, fun, and most importantly great tasting gelato.
+        </p>
+      </div>
+    </div>
+    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+      <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+        <h6 class="mb-2 font-semibold leading-5">Quality ingredients</h6>
+        <p class="text-sm text-gray-900">
+          With plenty of time on our hands, and a passion for ice cream and exotic ingredients, we began experimenting and developing recipes in our kitchen. 
+        </p>
+      </div>
+    </div>
+    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+      <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+        <h6 class="mb-2 font-semibold leading-5">South London</h6>
+        <p class="text-sm text-gray-900">
+          We are based at our production kitchen in South London and are committed to working with local suppliers, and being part of our local community.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!----Flavours---->
+<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div>
+      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        Brand new flavours
+      </p>
+    </div>
+    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+      <span class="relative inline-block">
+        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+          <defs>
+            <pattern id="7b568941-9ed0-4f49-85a0-5e21ca6c7ad6" x="0" y="0" width=".135" height=".30">
+              <circle cx="1" cy="1" r=".7"></circle>
+            </pattern>
+          </defs>
+          <rect fill="url(#7b568941-9ed0-4f49-85a0-5e21ca6c7ad6)" width="52" height="24"></rect>
+        </svg>
+        <span class="relative">The</span>
+      </span>
+	 gelato menu is chosen daily by our maestro
+    </h2>
+    <p class="text-base text-gray-700 md:text-lg">
+      Made in small batches & every day hand crafted by our skilled mastro gelataio.
+    </p>
+  </div>
+  <div class="grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2">
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/heather-barnes-bqafVjHkcAQ-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">The doctor said</h6>
+            <p class="text-sm text-gray-900">
+              Sportacus andrew weatherall goose Refined gentlemen super mario des lynam alpha trion zap rowsdower.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/tomek-vVsV0vkh0b0-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">Skate ipsum dolor</h6>
+            <p class="text-sm text-gray-900">
+              Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/tomek-Z54FRHZugQg-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">They urge you</h6>
+            <p class="text-sm text-gray-900">
+              A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/alana-harris-Ri35owZyfNU-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">Baseball ipsum dolor</h6>
+            <p class="text-sm text-gray-900">
+              Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/dana-devolk-5-RS_ScO3X4-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">The doctor said</h6>
+            <p class="text-sm text-gray-900">
+              Sportacus andrew weatherall goose Refined gentlemen super mario des lynam alpha trion zap rowsdower.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/heather-barnes-bqafVjHkcAQ-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">Skate ipsum dolor</h6>
+            <p class="text-sm text-gray-900">
+              Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/tomek-vVsV0vkh0b0-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">They urge you</h6>
+            <p class="text-sm text-gray-900">
+              A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="/" aria-label="View Item" class="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2">
+      <div class="flex flex-col h-full">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/tomek-Z54FRHZugQg-unsplash.jpg" class="object-cover w-full h-48" alt="" />
+        <div class="flex-grow border border-t-0 rounded-b">
+          <div class="p-5">
+            <h6 class="mb-2 font-semibold leading-5">Baseball ipsum dolor</h6>
+            <p class="text-sm text-gray-900">
+              Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy.
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+
+<?php
+get_footer();
---
+++ header.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( 'bg-white text-gray-900 antialiased' ); ?>>
+
+<?php do_action( 'tailpress_site_before' ); ?>
+
+<div id="page" class="min-h-screen flex flex-col">
+
+	<?php do_action( 'tailpress_header' ); ?>
+
+	<!-- <header> -->
+
+	
+
+		<!-- <div class="mx-auto container">
+			<div class="lg:flex lg:justify-between lg:items-center border-b py-6">
+				<div class="flex justify-between items-center">
+					<div>
+						<?php if ( has_custom_logo() ) { ?>
+                            <?php the_custom_logo(); ?>
+						<?php } else { ?>
+							<div class="text-lg uppercase">
+								<a href="<?php echo get_bloginfo( 'url' ); ?>" class="font-extrabold text-lg uppercase">
+									<?php echo get_bloginfo( 'name' ); ?>
+								</a>
+							</div>
+
+							<p class="text-sm font-light text-gray-600">
+								<?php echo get_bloginfo( 'description' ); ?>
+							</p>
+
+						<?php } ?>
+					</div>
+
+					<div class="lg:hidden">
+						<a href="#" aria-label="Toggle navigation" id="primary-menu-toggle">
+							<svg viewBox="0 0 20 20" class="inline-block w-6 h-6" version="1.1"
+								 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
+									<g id="icon-shape">
+										<path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"
+											  id="Combined-Shape"></path>
+									</g>
+								</g>
+							</svg>
+						</a>
+					</div>
+				</div>
+
+				<?php
+				wp_nav_menu(
+					array(
+						'container_id'    => 'primary-menu',
+						'container_class' => 'hidden bg-gray-100 mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent lg:block',
+						'menu_class'      => 'lg:flex lg:-mx-4',
+						'theme_location'  => 'primary',
+						'li_class'        => 'lg:mx-4',
+						'fallback_cb'     => false,
+					)
+				);
+				?>
+			</div>
+		</div>
+	</header>
+
+	<div id="content" class="site-content flex-grow"> -->
+
+<header>
+	<div class="bg-gray-900">
+  <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
+      <ul class="flex items-center hidden space-x-8 lg:flex">
+        <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">Product</a></li>
+        <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">Features</a></li>
+        <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">Pricing</a></li>
+      </ul>
+      <a href="/" aria-label="Company" title="Company" class="inline-flex items-center lg:mx-auto">
+        <svg class="w-8 text-teal-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+          <rect x="3" y="1" width="7" height="12"></rect>
+          <rect x="3" y="17" width="7" height="6"></rect>
+          <rect x="14" y="1" width="7" height="6"></rect>
+          <rect x="14" y="11" width="7" height="12"></rect>
+        </svg>
+		
+		<div class="flex items-center justify-center w-16 h-16 rounded-full bg-indigo-50">
+			<img class="w-12 h-12" src="<?php echo get_stylesheet_directory_uri() ?>/resources/images/FH_GELATO_1.png">
+        </div>
+      </a>
+      <!-- Mobile menu -->
+      <div class="ml-auto lg:hidden">
+        <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline">
+          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
+            <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
+            <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
+          </svg>
+        </button>
+        <!-- Mobile menu dropdown ---->
+        <div class="absolute top-0 left-0 w-full">
+          <div class="p-5 bg-white border rounded shadow-sm">
+            <div class="flex items-center justify-between mb-4">
+              <div>
+                <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
+                  <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+                    <rect x="3" y="1" width="7" height="12"></rect>
+                    <rect x="3" y="17" width="7" height="6"></rect>
+                    <rect x="14" y="1" width="7" height="6"></rect>
+                    <rect x="14" y="11" width="7" height="12"></rect>
+                  </svg>
+                  <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                </a>
+              </div>
+              <div>
+                <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                  <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <nav>
+              <ul class="space-y-4">
+                <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
+                <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
+                <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
+                
+              </ul>
+            </nav>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</header>
